7e2030db7493cac67418307a11bb57e4f9af6d60
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-10-06 12:58:23 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open (os.path.join (PATH, 'README.md'), encoding = 'utf-8') as f:
 
 setup (
         name = 'lb-pidsim-train',
-        version = '0.0.3',
+        version = '0.0.4',
         packages = find_packages(),
         package_data = {'data': ['Zmumu.root']},
         author = 'Matteo Barbetti',
