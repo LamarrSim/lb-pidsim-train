e4b95b2599ca503f8d556626eb6784182b7530c5
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-11-13 21:45:38 +0100

    added data handler

--- lb_pidsim_train/trainers/__init__.py
+++ lb_pidsim_train/trainers/__init__.py
@@ -1,4 +1,5 @@
 from .BaseTrainer      import BaseTrainer
+from .DataHandler      import DataHandler
 from .TensorTrainer    import TensorTrainer
 from .GanTrainer       import GanTrainer
 from .ScikitClassifier import ScikitClassifier
