f4be7bf7f326e0900785420c84c527bca82447cb
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-11-13 22:17:00 +0100

    fixed bugs

--- lb_pidsim_train/trainers/__init__.py
+++ lb_pidsim_train/trainers/__init__.py
@@ -1,5 +1,5 @@
-from .BaseTrainer      import BaseTrainer
 from .DataHandler      import DataHandler
+from .BaseTrainer      import BaseTrainer
 from .TensorTrainer    import TensorTrainer
 from .GanTrainer       import GanTrainer
 from .ScikitClassifier import ScikitClassifier
