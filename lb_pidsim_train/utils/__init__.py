f9cd981e8e71e41e6b1be2cb5962663c6cb7842b
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2021-11-13 01:18:55 +0100

    added argparser

--- lb_pidsim_train/utils/__init__.py
+++ lb_pidsim_train/utils/__init__.py
@@ -1,3 +1,4 @@
+from .argparser               import argparser
 from .CustomColumnTransformer import CustomColumnTransformer
 from .data_from_trees         import data_from_trees
 from .getBinPDFs              import getBinPDFs
